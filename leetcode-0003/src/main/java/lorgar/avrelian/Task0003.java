df2ce7bb21d11dd3ebba228dddcc8f903ef67494
lorgar-avrelian <victor-14-244@mail.ru>
2025-03-24 22:23:30 +0300

    Task 3 rework

--- leetcode-0003/src/main/java/lorgar/avrelian/Task0003.java
+++ leetcode-0003/src/main/java/lorgar/avrelian/Task0003.java
@@ -1,6 +1,9 @@
 package lorgar.avrelian;
 
+import java.util.ArrayDeque;
+
 public class Task0003 {
+
     public static void main(String[] args) {
         String s = "pwwkew";
         int result = lengthOfLongestSubstring(s);
@@ -8,43 +11,20 @@ public class Task0003 {
     }
 
     private static int lengthOfLongestSubstring(String s) {
-        if (!s.isEmpty()) {
-            int total = 0;
-            int count = 1;
-            char[] charArray = s.toCharArray();
-            for (int i = 0; i < charArray.length; i++) {
-                StringBuilder sb = new StringBuilder(charArray[i] + "");
-                if (i < charArray.length - 1) {
-                    for (int j = i + 1; j < charArray.length; j++) {
-                        if (j == charArray.length - 1) {
-                            if (sb.indexOf(charArray[j] + "") == -1) count++;
-                            if (count >= total) {
-                                total = count;
-                            }
-                            count = 1;
-                            break;
-                        }
-                        if (sb.indexOf(charArray[j] + "") == -1) {
-                            sb.append(charArray[j]);
-                            count++;
-                        } else {
-                            if (count >= total) {
-                                total = count;
-                            }
-                            count = 1;
-                            break;
-                        }
-                    }
-                } else {
-                    if (count >= total) {
-                        total = count;
-                    }
-                    count = 1;
+        ArrayDeque<Character> queue = new ArrayDeque<>();
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (queue.contains(ch)) {
+                while (!queue.isEmpty() && queue.peek() != ch) {
+                    queue.remove();
                 }
+                queue.remove();
             }
-            return total;
-        } else {
-            return 0;
+            queue.add(ch);
+            result = Math.max(result, queue.size());
+
         }
+        return result;
     }
 }
